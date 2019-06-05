d7861d7713e25a617db6eb9e438a051f7591c5d7
Juliusz Waluszewski <shirajira@gmail.com>
2019-06-05 19:23:28 +0200

    poprawiono na dane z listy

--- src/main/java/compilator/przyklad/Main.java
+++ src/main/java/compilator/przyklad/Main.java
@@ -10,20 +10,21 @@ public class Main {
 
         List<Osoba> osoba = new ArrayList<Osoba>();
 
-        Osoba o1 = new Osoba("imie", "nazwisko",  19);
-        Osoba o2 = new Osoba("d", "nazwisko",  11);
-        Osoba o3 = new Osoba("imie", "a",  25);
-        Osoba o4 = new Osoba("x", "nazwisko",  34);
-        Osoba o5 = new Osoba("v", "nazwisko",  50);
-        Osoba o7 = new Osoba("imie", "7",  13);
-        Osoba o8 = new Osoba("t", "nazwisko",  10);
+        osoba.add(new Osoba("imie", "nazwisko", 19));
+        osoba.add(new Osoba("d", "nazwisko", 11));
+        osoba.add(new Osoba("imie", "a", 25));
+        osoba.add(new Osoba("x", "nazwisko", 34));
+        osoba.add(new Osoba("v", "nazwisko", 50));
+        osoba.add(new Osoba("imie", "7", 13));
+        osoba.add(new Osoba("t", "nazwisko", 10));
 
 
-        Collections.sort(osoba, (os1, os2) -> Integer.compare(os1.getWiek(), os2.getWiek()));
+
+                Collections.sort(osoba, (os1, os2) -> Integer.compare(os1.getWiek(), os2.getWiek()));
+
+        System.out.println(osoba);
 
     }
-
-
 
 
 }
